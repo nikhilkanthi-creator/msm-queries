134df7b98ed43a769756319f29a14f092d35669f
Nikhil Kanthi <nkanthi@chicagobooth.edu>
2025-05-03 20:16:02 +0000

    movie list done

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   get("/movies", { :controller => "movies", :action => "index"})
 
-  get("/movies/:the_id" { :controller => "movies", :action => "show"})
-  
+  get("/movies/:the_id", { :controller => "movies", :action => "show"})
+
 end
