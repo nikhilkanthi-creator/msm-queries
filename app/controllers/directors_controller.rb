01573ddc9ec2ae9908939431d2972bdee3d506ef
Nikhil Kanthi <nkanthi@chicagobooth.edu>
2025-05-03 21:13:09 +0000

    finished!

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -10,4 +10,21 @@ class DirectorsController < ApplicationController
     @the_director = matching_records.at(0)
     render({ :template => "director_templates/details"})
   end
+
+  def youngest
+    @director_age = Director.where.not({ :dob => nil }).order({ :dob => :desc})
+    @director_youngest = @director_age.at(0)
+    @youngest_name = @director_youngest.name
+    @youngest_dob = @director_youngest.dob
+    render({ :template => "director_templates/youngest"})
+  end
+
+  def eldest
+    @director_age = Director.all.order(:dob)
+    @director_eldest = @director_age.at(0)
+    @eldest_name = @director_eldest.name
+    @eldest_dob = @director_eldest.dob
+    render({ :template => "director_templates/eldest"})
+  end
+
 end
